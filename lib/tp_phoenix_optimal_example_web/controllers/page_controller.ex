e4dff5c8396eee1e08a7912df91711257957dffc
James Byrne <jamesdesbyrne@gmail.com>
2018-01-23 11:49:06 +0000

    Adding a screen and handling for optimal order errors

--- lib/tp_phoenix_optimal_example_web/controllers/page_controller.ex
+++ lib/tp_phoenix_optimal_example_web/controllers/page_controller.ex
@@ -4,29 +4,26 @@ defmodule TpPhoenixOptimalExampleWeb.PageController do
 
   def index(conn, _params) do
     # Create a new order
-    {:ok, %HTTPoison.Response{body: body}} = Optimal.create_order
-
-    # Retrieve the uri from the struct
-    {:ok, %{"link" => [%{"uri" => uri}|_]}} = Poison.decode(body)
-
-    render(conn, "index.html", uri: uri)
+    {:ok, %HTTPoison.Response{body: body}} = Optimal.create_order()
+    render_index(conn, Poison.decode(body))
   end
 
-  def success_redirect(conn, %{"transaction" => transaction}) do
-    render(conn, "success_redirect.html", transaction: transaction)
-  end
+  # Retrieve the uri from the struct
+  def render_index(conn, {:ok, %{"link" => [%{"uri" => uri}|_]}}),
+    do: render(conn, "index.html", data: %{"uri" => uri})
 
-  def error_redirect(conn, %{"transaction" => transaction}) do
-    render(conn, "error_redirect.html", transaction: transaction)
-  end
+  def render_index(conn, {:ok, %{"error" => error}}),
+    do: render(conn, "index.html", data: %{"error" => error})
 
-  def success_callback(conn, %{"transaction" => transaction}) do
-    IO.inspect transaction
-    render(conn, "index.json", %{})
-  end
+  def success_redirect(conn, %{"transaction" => transaction}),
+    do: render(conn, "success_redirect.html", data: %{"transaction" => transaction})
 
-  def error_callback(conn, %{"transaction" => transaction}) do
-    IO.inspect transaction
-    render(conn, "index.json", %{})
-  end
+  def error_redirect(conn, %{"transaction" => transaction}),
+    do: render(conn, "error_redirect.html", data: %{"transaction" => transaction})
+
+  def success_callback(conn, %{"transaction" => transaction}),
+    do: render(conn, "index.json", %{})
+
+  def error_callback(conn, %{"transaction" => transaction}),
+    do: render(conn, "index.json", %{})
 end
